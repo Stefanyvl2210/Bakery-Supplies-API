ba616d1c003a37b249db723e0c114f71415bd82c
Luis Vargas <luisf55horn@gmail.com>
2022-08-08 22:47:13 -0400

    user role implementation

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,30 +4,28 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
-{
+class CreateUsersTable extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
+    public function up() {
         //missing role_id
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create( 'users', function ( Blueprint $table ) {
             $table->id();
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('phone_number');
-            $table->string('image')->nullable();
-            $table->string('verification_code')->nullable();
+            $table->string( 'first_name' );
+            $table->string( 'last_name' );
+            $table->string( 'email' )->unique();
+            $table->timestamp( 'email_verified_at' )->nullable();
+            $table->string( 'password' );
+            $table->string( 'phone_number' );
+            $table->string( 'image' )->nullable();
+            $table->string( 'verification_code' )->nullable();
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
-        });
+        } );
     }
 
     /**
@@ -35,8 +33,7 @@ class CreateUsersTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
-        Schema::dropIfExists('users');
+    public function down() {
+        Schema::dropIfExists( 'users' );
     }
 }
