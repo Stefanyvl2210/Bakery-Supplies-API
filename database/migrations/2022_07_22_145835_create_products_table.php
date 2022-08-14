5a2be87aae48a76fa1eefb71a936a78d30898d9a
Luis Vargas <luisf55horn@gmail.com>
2022-08-14 16:48:49 -0400

    fix

--- database/migrations/2022_07_22_145835_create_products_table.php
+++ database/migrations/2022_07_22_145835_create_products_table.php
@@ -4,25 +4,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProductsTable extends Migration
-{
+class CreateProductsTable extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
-        Schema::create('products', function (Blueprint $table) {
+    public function up() {
+        Schema::create( 'products', function ( Blueprint $table ) {
             $table->id();
-            $table->string('name')->unique();
-            $table->string('description');
-            $table->string('image');
-            $table->float('price');
-            $table->integer('quantity_available')->default(0);
+            $table->string( 'name' )->unique();
+            $table->string( 'description' );
+            $table->string( 'image' )->nullable();
+            $table->float( 'price' );
+            $table->integer( 'quantity_available' )->default( 0 );
             $table->timestamps();
             $table->softDeletes();
-        });
+        } );
     }
 
     /**
@@ -30,8 +28,7 @@ class CreateProductsTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
-        Schema::dropIfExists('products');
+    public function down() {
+        Schema::dropIfExists( 'products' );
     }
 }
--- database/migrations/2022_07_22_145949_create_orders_table.php
+++ database/migrations/2022_07_22_145949_create_orders_table.php
@@ -4,46 +4,47 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateOrdersTable extends Migration
-{
+class CreateOrdersTable extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
-        Schema::create('orders', function (Blueprint $table) {
+    public function up() {
+        Schema::create( 'orders', function ( Blueprint $table ) {
             $table->id();
-            $table->binary('is_guest');
-            $table->string('guest_data')->nullable();
-            $table->string('guest_address')->nullable();
-            $table->string('status');
-            $table->string('delivery_type');
-            $table->float('total');
+            $table->binary( 'is_guest' );
+            $table->string( 'guest_data' )->nullable();
+            $table->string( 'guest_address' )->nullable();
+            $table->string( 'status' );
+            $table->string( 'delivery_type' );
+            $table->float( 'taxes' )->default( 0 );
+            $table->float( 'total' );
+            $table->timestamp( 'estimate_delivery' )->nullable();
+            $table->timestamp( 'delivery_time' )->nullable();
             $table->timestamps();
             $table->softDeletes();
-        });
-        Schema::table('orders', function (Blueprint $table) {
-            $table->unsignedBigInteger('user_id')->nullable();
-            $table->foreign('user_id')
-                ->references('id')
-                ->on('users')
-                ->onUpdate('cascade')
-                ->onDelete('cascade');
-            $table->unsignedBigInteger('payment_id')->nullable();
-            $table->foreign('payment_id')
-                ->references('id')
-                ->on('payments')
-                ->onUpdate('cascade')
-                ->onDelete('cascade');
-            $table->unsignedBigInteger('address_id')->nullable();
-            $table->foreign('address_id')
-                ->references('id')
-                ->on('addresses')
-                ->onUpdate('cascade')
-                ->onDelete('cascade');
-        });
+        } );
+        Schema::table( 'orders', function ( Blueprint $table ) {
+            $table->unsignedBigInteger( 'user_id' )->nullable();
+            $table->foreign( 'user_id' )
+                ->references( 'id' )
+                ->on( 'users' )
+                ->onUpdate( 'cascade' )
+                ->onDelete( 'cascade' );
+            $table->unsignedBigInteger( 'payment_id' )->nullable();
+            $table->foreign( 'payment_id' )
+                ->references( 'id' )
+                ->on( 'payments' )
+                ->onUpdate( 'cascade' )
+                ->onDelete( 'cascade' );
+            $table->unsignedBigInteger( 'address_id' )->nullable();
+            $table->foreign( 'address_id' )
+                ->references( 'id' )
+                ->on( 'addresses' )
+                ->onUpdate( 'cascade' )
+                ->onDelete( 'cascade' );
+        } );
     }
 
     /**
@@ -51,16 +52,15 @@ class CreateOrdersTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
-        Schema::table('orders', function (Blueprint $table) {
-            $table->dropForeign(['user_id']);
-            $table->dropColumn('user_id');
-            $table->dropForeign(['payment_id']);
-            $table->dropColumn('payment_id');
-            $table->dropForeign(['address_id']);
-            $table->dropColumn('address_id');
-        });
-        Schema::dropIfExists('orders');
+    public function down() {
+        Schema::table( 'orders', function ( Blueprint $table ) {
+            $table->dropForeign( ['user_id'] );
+            $table->dropColumn( 'user_id' );
+            $table->dropForeign( ['payment_id'] );
+            $table->dropColumn( 'payment_id' );
+            $table->dropForeign( ['address_id'] );
+            $table->dropColumn( 'address_id' );
+        } );
+        Schema::dropIfExists( 'orders' );
     }
 }
